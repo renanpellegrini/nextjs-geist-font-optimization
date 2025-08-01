a54831e663388211bf40e3da7709642ab888f333
Renan Lucas Pellegrini <117469929+renanpellegrini@users.noreply.github.com>
2025-08-01 15:04:48 -0300

    Add .next/static/chunks/src_app_books_page_tsx_650e26a1._.js 1754071487917

---
+++ .next/static/chunks/src_app_books_page_tsx_650e26a1._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_49dab706._.js",
+  "static/chunks/node_modules_a59898e7._.js"
+],
+    source: "dynamic"
+});
